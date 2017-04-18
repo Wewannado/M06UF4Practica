7dcef70d0848f607cfbfb14276125818c3fb1ebb
Roger G. Coscojuela <rogilico@gmail.com>
2017-04-18 20:33:54 +0200

    Projecte Asseguradora

---
+++ src/m06uf4practica/M06UF4Practica.java
@@ -0,0 +1,19 @@
+/*
+ * Author Roger G. Coscojuela
+ */
+package m06uf4practica;
+
+/**
+ *
+ * @author Roger G. Coscojuela
+ */
+public class M06UF4Practica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
