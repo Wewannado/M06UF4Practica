fc110f9a11eaeca312f2d9b9d12f38c92d76bc3c
AbelSerranoDAM <abeleitor6@gmail.com>
2017-04-20 20:07:58 +0200

    ey

--- src/model/UnitatFormativa.java
+++ src/model/UnitatFormativa.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -21,40 +22,43 @@ import javax.persistence.Table;
 @Table(name = "M6UF4_UF")
 public class UnitatFormativa implements Serializable {
 
-    @ManyToOne
-    private Matricula matricula;
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long idUF;
 
     private String nom;
     private int hores;
+    @JoinColumn(name = "idCurs")
+    private UnitatFormativa uf;
+    @JoinColumn(name = "idModul")
+    private Modul modul;
+    @JoinColumn(name = "idMatricula")
+    private Matricula matricula;
 
-    
     public Long getId() {
-        return id;
+        return idUF;
     }
 
     public void setId(Long id) {
-        this.id = id;
+        this.idUF = id;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (idUF != null ? idUF.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        // TODO: Warning - this method won't work in the case the idUF fields are not set
         if (!(object instanceof UnitatFormativa)) {
             return false;
         }
         UnitatFormativa other = (UnitatFormativa) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.idUF == null && other.idUF != null) || (this.idUF != null && !this.idUF.equals(other.idUF))) {
             return false;
         }
         return true;
@@ -62,7 +66,7 @@ public class UnitatFormativa implements Serializable {
 
     @Override
     public String toString() {
-        return "model.UnitatFormativa[ id=" + id + " ]";
+        return "model.UnitatFormativa[ id=" + idUF + " ]";
     }
 
     public String getNom() {
